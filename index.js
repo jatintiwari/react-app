1fd90f37f72a24ebfb593214997c05a65a28101c
jatin-tiwari <jatin.tiwari@flipkart.com>
2018-05-11 19:15:55 +0530

    mock api

--- index.js
+++ index.js
@@ -25,10 +25,33 @@ app.engine('html', ejs.renderFile);
 app.use('/bundle.js', function (req, res, next) {
   res.sendFile(path.join(__dirname, "dist", "bundle.js"));
 });
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', "*")
+    .header('Access-Control-Allow-Headers', "Origin, X-Requested-With, Accept, Content-Type");
+  next();
+})
+
+app.use('/api/tweets', function (req, res, next) {
+  var json = () => {
+    const arr = [];
+    let n = 0;
+    while(n < 100) {
+      arr.push({ text: Math.round(Math.random()*100) });
+      n++;
+    }
+    return arr;
+  };
+  res.json(json()).status(200);
+});
+app.use('/api/*', function (req, res, next) {
+  res.status(400).send({ error: 'Error message' });
+});
+
 app.use('/', function (req, res, next) {
   res.render('index.html', {});
 });
 
 app.listen(app.get('port'), function () {
-	console.log('Express server is up on port 3000');
+  console.log('Express server is up on port 3000');
 });
